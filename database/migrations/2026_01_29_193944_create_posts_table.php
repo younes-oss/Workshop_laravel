6fdc962cc69116517f4017feece3d2334c9551e9
laaouafi ishak <laaouafi6@gmail.com>
2026-01-30 11:04:14 +0100

    migrations and models

--- database/migrations/2026_01_29_193944_create_posts_table.php
+++ database/migrations/2026_01_29_193944_create_posts_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -13,10 +14,12 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->text('discription');
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
-
+    
     /**
      * Reverse the migrations.
      */
