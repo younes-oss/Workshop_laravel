e42522ad590a12879c12cfb10cdfc250146ce231
laaouafi ishak <laaouafi6@gmail.com>
2026-01-30 15:09:37 +0100

    commit before m

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -60,7 +60,7 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $post->update([$request->all()]);
     }
 
     /**
